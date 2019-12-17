fe092c250792810cd759fbf389ee7e6ae4eb1cf4
unknown <g.mameniskis1@gmail.com>
2019-12-17 15:34:07 -0500

    learned a thing or two

--- src/main/java/com/github/curriculeon/MainApplication.java
+++ src/main/java/com/github/curriculeon/MainApplication.java
@@ -4,5 +4,14 @@ public class MainApplication {
     public static void main(String[] args) {
         Runnable myObject = new MyObject();
         myObject.run();
+        Person human = new Person();
+
+        human.setFirstName("Felix");
+        human.setLastName("Leone");
+
+        System.out.println("My first name is " + human.getFirstName());
+        System.out.println("and my last name is " + human.getLastName());
+
+        Person human1 = new Person("hmhjghj", "fdhf");
     }
 }
