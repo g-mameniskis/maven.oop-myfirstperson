488e436172db26a31aaf47034d53c15b7315ba9c
unknown <g.mameniskis1@gmail.com>
2019-12-18 11:20:12 -0500

    annotated areas that lead to my confusion

--- src/main/java/com/github/curriculeon/MainApplication.java
+++ src/main/java/com/github/curriculeon/MainApplication.java
@@ -4,14 +4,20 @@ public class MainApplication {
     public static void main(String[] args) {
         Runnable myObject = new MyObject();
         myObject.run();
+        // creating an instance of the Person class (human).
         Person human = new Person();
-
+        // this sets the value for my setters in Person.java
         human.setFirstName("Felix");
         human.setLastName("Leone");
 
-        System.out.println("My first name is " + human.getFirstName());
-        System.out.println("and my last name is " + human.getLastName());
+        System.out.println("Behold! The beauty of Encapsulation.");
+        System.out.println("--------------------------------------");
+        // this calls my getter which is designed to return the value of my local variables of class Person
+        System.out.println("My first name is --> " + human.getFirstName());
+        System.out.println("and my last name is --> " + human.getLastName());
 
+        // if I decided to use the constructor (not actually a constructor) in Person.java, this is how
+        // i would create an instance
         Person human1 = new Person("hmhjghj", "fdhf");
     }
 }
--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -4,20 +4,23 @@ package com.github.curriculeon;
  * Created by leon on 12/16/2019.
  */
 public class Person {
+    // these are my fields/local variables. it's very bad practice to give them values at this stage.
+    // it's a good idea to make these private so that they can't be accessed outside of this class
     private String firstName;
     private String lastName;
-
+    // this acts as my constructor. this associates my fields (state) with the parameters being passed.
     public Person(String firstName, String lastName) {
        this.firstName = firstName;
        this.lastName = lastName;
     }
-
+    // this is another way to do line 11-13. I believe this is comparatively better practice.
     public Person(){}
-
+    // Now let's get into Encapsulation:
+    // this is my first getter. it returns the value of my variable, in this case firstName
     public String getFirstName() {
         return this.firstName;
     }
-
+    // this is my setter. it takes a parameter (firstName) and assigns it to the firstName local variable
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
